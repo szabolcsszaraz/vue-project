c16c44fb2773480a59e594933409e1a707c4eb14
szabolcsszaraz <szarazszabolcs22@gmail.com>
2024-12-30 14:29:18 +0100

    pridavanie bootstrap a bootstrap-icon

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
